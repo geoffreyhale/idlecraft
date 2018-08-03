db1ff2771291daadb94d941fce091bc67b15d424
geoffreyhale <geoffreyhale@gmail.com>
2018-08-03 00:25:28 -0700

    add water

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function Resource() {
+function Resource(name) {
+    this.name = name;
     this.quantity = 0;
 }
 
-const time = new Resource();
-const trees = new Resource();
-const wood = new Resource();
-const fire = new Resource();
-const heat = new Resource();
+const time = new Resource('time');
+const trees = new Resource('trees');
+const wood = new Resource('wood');
+const fire = new Resource('fire');
+const heat = new Resource('heat');
+const water = new Resource('water');
 
 time.update = function() {
     time.quantity++;
@@ -27,12 +29,22 @@ heat.update = function() {
         heat.quantity -= 1;
     }
 };
+water.update = function() {
+    if (heat.quantity > 0 && time.quantity % 20 == 0) {
+        water.quantity += 1;
+        water.name = 'water';
+    } else if (heat.quantity <= 0) {
+        water.quantity = 0;
+        water.name = 'ice';
+    }
+};
 
 function update() {
     time.update();
     trees.update();
     fire.update();
     heat.update();
+    water.update();
 }
 
 function chopTrees() {
@@ -69,8 +81,14 @@ function pauseLoop() {
 function ResourceEl(domId, resource) {
     this.resource = resource;
     this.elQuantity = document.querySelector('#' + domId + ' > .quantity');
+    this.elName = document.querySelector('#' + domId + ' > .name');
+    this.prevName = null;
     this.draw = function() {
         this.elQuantity.innerHTML = this.resource.quantity;
+
+        if (resource.name != this.prevName) {
+            this.elName.innerHTML = this.resource.name.charAt(0).toUpperCase() + this.resource.name.slice(1);
+        }
     };
 }
 
@@ -80,6 +98,7 @@ window.onload = function(){
     const elWood = new ResourceEl('wood', wood);
     const elFire = new ResourceEl('fire', fire);
     const elHeat = new ResourceEl('heat', heat);
+    const elWater = new ResourceEl('water', water);
 
     function draw() {
         elTime.draw();
@@ -87,6 +106,7 @@ window.onload = function(){
         elWood.draw();
         elFire.draw();
         elHeat.draw();
+        elWater.draw();
     }
 
     const elStartButton = document.getElementById('start');
